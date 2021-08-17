fd2bec26e9f1abf2d5776192c92d43372f31b47b
mrsovad <serov-pa@yandex.ru>
2021-08-17 13:56:45 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+//const fs = require('fs');
+const express = require('express');
+const app = express();
+let httpsServer = require('http').Server(app);
+let io = require('socket.io')(httpsServer);
+
+var port = 3000
+var CircularJSON = require('circular-json');
+
+var r = 'default'
+
+app.get('/', function (request, response, next) {
+    response.send(CircularJSON.stringify(io));
+});
+
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+io.on('connection', function (socket) {
+    r = socket.handshake.query.room
+
+    socket.join(r);
+
+    socket.on('voice', function(message) {
+        // can choose to broadcast it to whoever you want
+        io.to(message.room).emit('voice', message.blob);
+    });
+
+    //add name user
+    socket.on('add nameuser', function (user) {
+        room = {};
+        for (key in io.sockets.adapter.rooms) {
+            if (user.id === key) {
+                io.sockets.adapter.rooms[key].name = user.name;
+            }
+        }
+        for (key in io.sockets.adapter.rooms) {
+            if (key === r) {
+                var sockets = {};
+
+                for (var k in io.sockets.adapter.rooms[key].sockets) {
+                    sockets[k] = io.sockets.adapter.rooms[k].name;
+                }
+                room[key] = {
+                    'sockets': sockets
+                }
+            }
+        }
+        //connect user
+        io.to(r).emit('user joined',{
+            socketId: socket.id,
+            room: room,
+            params: socket.handshake.query.room
+        });
+    });
+
+    //client message
+    socket.on('chat.message', function (message) {
+        io.to(message.room).emit('chat.message', message.message);
+    });
+
+    //client sends user typing event server
+    socket.on('user typing', function (username) {
+        io.to(username.room).emit('user typing', username.id);
+    });
+
+    //clients stopped typing
+    socket.on('stopped typing', function (username) {
+        io.to(username.room).emit('stopped typing', username.id);
+    });
+
+    socket.on('disconnect', function () {
+        console.log('User left: ' + socket.id);
+        //disconnected
+        socket.broadcast.emit('user left', socket.id);
+    })
+
+});
+
+
+httpsServer.listen(port)
+httpsServer.on('listening', function() {
+    console.log('Express server started on port %s', httpsServer.address().port);
+});
---
+++ public/client.js
@@ -0,0 +1,180 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+// var socket = io('http://127.0.0.1:3000', {query: "room="+urlParams.get('room')})
+var socket = io('https://socket.local.dv', {query: "room="+urlParams.get('room')})
+//var socket = io().connect('http://127.0.0.1:3000', {query: "room="+urlParams.get('room')});
+
+function in_array(what, where) {
+    for (var i = 0; i < where.length; i++)
+        if (what == where[i].id)
+            return true;
+    return false;
+}
+
+new Vue({
+    el: '#app',
+    data: {
+        connectedUsers: [],
+        messages: [],
+        message: {
+            'type': '',
+            'action': '',
+            'user': '',
+            'text': '',
+            'timestamp': '',
+            'reg': false
+        },
+        room:'',
+        areTyping: []
+    },
+    created: function () {
+        var constraints = { audio: true };
+        navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream) {
+            var mediaRecorder = new MediaRecorder(mediaStream);
+            mediaRecorder.onstart = function(e) {
+                this.chunks = [];
+            };
+            mediaRecorder.ondataavailable = function(e) {
+                this.chunks.push(e.data);
+            };
+            mediaRecorder.onstop = function(e) {
+                var blob = new Blob(this.chunks, { 'type' : 'audio/ogg; codecs=opus' });
+                socket.emit('radio', {blob: blob, room:this.room});
+            };
+
+            // Start recording
+            mediaRecorder.start();
+
+            // Stop recording after 5 seconds and broadcast it to server
+            setInterval(function() {
+                mediaRecorder.stop()
+                mediaRecorder.start()
+            }, 5000);
+        });
+
+        // When the client receives a voice message it will play the sound
+        socket.on('voice', function(arrayBuffer) {
+            var blob = new Blob([arrayBuffer], { 'type' : 'audio/ogg; codecs=opus' });
+            var audio = document.createElement('audio');
+            audio.src = window.URL.createObjectURL(blob);
+            audio.play();
+        });
+
+
+        //if server emits user joined, update connectedUsers array
+        socket.on('user joined', function (response) {
+            console.log(response);
+            let data = response.room;
+            let socketId = response.socketId
+            for (key in data) {
+                this.room = key;
+                for (var k in data[key].sockets) {
+                    if (!in_array(k, this.connectedUsers)) {
+                        this.connectedUsers.push({id: k, name: data[key].sockets[k]});
+                    }
+                }
+            }
+            for (key in this.connectedUsers) {
+                if (this.connectedUsers[key].id == socketId) {
+                    var infoMsg = {
+                        "type": "info",
+                        "msg": "Пользователь " + this.connectedUsers[key].name + " Присоединился"
+                    }
+                    this.messages.push(infoMsg);
+                }
+            }
+        }.bind(this));
+
+        //if caht.message update messages array
+        socket.on('chat.message', function (message) {
+            this.messages.push(message);
+        }.bind(this));
+
+        //server emit user typing
+        socket.on('user typing', function (username) {
+            this.areTyping.push(username);
+        }.bind(this));
+
+        //server emits stoped typing
+        socket.on('stopped typing', function (username) {
+            var index = this.areTyping.indexOf(username);
+            if (index >= 0) {
+                this.areTyping.splice(index, 1);
+            }
+        }.bind(this));
+
+
+        //if server broadcast 'user left' remuve leaving users connected users
+        socket.on('user left', function (socketId) {
+            for (key in this.connectedUsers) {
+                if (this.connectedUsers[key].id == socketId) {
+                    var infoMsg = {
+                        "type": "info",
+                        "msg": "Пользователь " + this.connectedUsers[key].name + " покинул чат"
+
+                    }
+                    this.messages.push(infoMsg);
+                    this.connectedUsers.splice(key, 1);
+                }
+            }
+
+        }.bind(this));
+
+        //document.getElementById('panB').scrollTop = 10000000000000; offsetHeight
+    },
+    methods: {
+        send: function () {
+            for (key in this.connectedUsers) {
+                if (this.connectedUsers[key].id == socket.id) {
+                    this.message.user = this.connectedUsers[key].name;
+                }
+            }
+            this.message.type = "chat";
+            lt = new Date();
+            Hour = lt.getHours();
+            Minutes = lt.getMinutes();
+            this.message.timestamp = Hour + ':' + Minutes;
+            socket.emit('chat.message', {message: this.message, room:this.room});
+            this.message.type = '';
+            this.message.user = '';
+            this.message.text = '';
+            this.message.timastamp = '';
+
+            $("#messages").bind('DOMSubtreeModified', function () { // отслеживаем изменение содержимого блока
+                document.getElementById('panB').scrollTop = 10000000000000000000000000;
+            });
+        },
+        sendReg: function (res) {
+            this.message.reg = true;
+            socket.emit('add nameuser', {name: res.srcElement.elements.username.value, id: socket.id});
+        },
+        userIsTyping: function (username) {
+            if (this.areTyping.indexOf(username) >= 0) {
+                $('.pencil').animate({left: '-10px'}, 1000);
+                $('.pencil').animate({left: '0px'}, 1000);
+                return true;
+            }
+            return false;
+        },
+        usersAreTyping: function () {
+            if (this.areTyping.indexOf(socket.id) <= -1) {
+                this.areTyping.push(socket.id);
+                socket.emit('user typing', {id:socket.id, room:this.room});
+            }
+        },
+        stoppedTyping: function (keycode) {
+            if (keycode == '13' || (keycode == '8' && this.message.text == '')) {
+                var index = this.areTyping.indexOf(socket.id);
+                if (index >= 0) {
+                    this.areTyping.splice(index, 1);
+                    socket.emit('stopped typing', {id:socket.id,room:this.room});
+                }
+            }
+        },
+        smile: function (event) {
+            var val = $("#outMes").val();
+            this.message.text = val + ' ' + event.srcElement.outerHTML + ' ';
+        }
+
+    }
+});
